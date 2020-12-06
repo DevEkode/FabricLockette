ead092cc5ae9393f71f6737e16763c61e35b55b3
Ekode <ekode@protonmail.com>
2020-12-06 12:08:05 +0100

    Making translation work with RessourceBundles

--- src/main/java/fr/ekode/fabriclockette/core/PlayerHelper.java
+++ src/main/java/fr/ekode/fabriclockette/core/PlayerHelper.java
@@ -1,5 +1,6 @@
 package fr.ekode.fabriclockette.core;
 
+import fr.ekode.fabriclockette.core.lang.Lang;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -10,7 +11,7 @@ public class PlayerHelper {
     private PlayerHelper() {}
 
     public static void sendAccessDeniedMessage(PlayerEntity player){
-        player.sendMessage(new TranslatableText("block.fabriclockette.denied"), true);
+        player.sendMessage(new Lang().withKey("block.fabriclockette.denied"), true);
         player.playSound(SoundEvents.ENTITY_VILLAGER_NO, SoundCategory.BLOCKS, 1.0F, 1.0F);
     }
 }
