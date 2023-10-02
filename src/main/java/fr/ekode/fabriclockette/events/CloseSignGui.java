399b4aed67711c39c53c1e3eaf755700941a4f6d
Ekode <ekode@protonmail.com>
2023-10-02 21:06:49 +0200

    Fixed some case when sign cannot be broken when invalid

--- src/main/java/fr/ekode/fabriclockette/events/CloseSignGui.java
+++ src/main/java/fr/ekode/fabriclockette/events/CloseSignGui.java
@@ -1,6 +1,8 @@
 package fr.ekode.fabriclockette.events;
 
 import fr.ekode.fabriclockette.core.PlayerHelper;
+import fr.ekode.fabriclockette.entities.BlockStatePos;
+import fr.ekode.fabriclockette.managers.ContainerManager;
 import fr.ekode.fabriclockette.managers.SignManager;
 import net.minecraft.util.ActionResult;
 
@@ -13,18 +15,32 @@ public class CloseSignGui implements EventRegistrator {
     public void register() {
         // When a player close a sign GUI
         CloseSignGuiCallback.EVENT.register((sign, player) -> {
+            // Skip if sign is not private
             SignManager signManager = new SignManager(sign);
+            if(!signManager.isSignPrivate()) return ActionResult.PASS;
 
-            if (signManager.isSignPrivate() && signManager.getAttachedContainer() != null) {
+            // Skip if container does not exists
+            BlockStatePos container = signManager.getAttachedContainer();
+            if(container == null) return ActionResult.PASS;
+
+            // Check if player own this container or if it's not protected
+            ContainerManager containerManager = new ContainerManager(sign.getWorld(),container.getBlockPos());
+            if(containerManager.isOwner(player) || !containerManager.isProtected()){
+                // Success
                 signManager.populateSignUuids();
 
                 if (signManager.hasOwners()) {
                     signManager.formatSign();
                     PlayerHelper.sendBlockProtectedMessage(player);
                 }
-            } else {
+            }else{
+                // Error
                 signManager.removeSignOwners();
+                // Destroy sign
+                sign.getWorld().breakBlock(sign.getPos(),true);
+                PlayerHelper.sendAccessDeniedMessage(player);
             }
+
             return ActionResult.PASS;
         });
     }
